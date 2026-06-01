8aea020a3fdef6501c1fb30912886f792d377caf
lonixgrifer-afk <lonixgrifer@gmail.com>
2026-06-01 14:13:42 +0500

    Update max.py

--- max.py
+++ max.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta, timezone
 
 # Один файл, без requirements.txt и .env.
 # Заполните перед запуском. Можно также передать через переменные окружения.
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8680736365:AAGH9QWkNshyIlD8giWHhm93xKR26p7sCiE")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8854265866:AAGMNw0kEatEuk-DZWbdVUSu98kL-hulQ-g")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "change-this-password")
 DB_PATH = os.getenv("DB_PATH", "bot.db")
 
@@ -33,7 +33,7 @@ BACK_BUTTON_CUSTOM_EMOJI_ID = os.getenv("BACK_BUTTON_CUSTOM_EMOJI_ID", "54272429
 BUTTON_STYLES_JSON = os.getenv("BUTTON_STYLES", "{}")
 
 # Если список пустой, первый вошедший пользователь автоматически станет админом.
-ADMIN_TELEGRAM_IDS = [8949311928]
+ADMIN_TELEGRAM_IDS = [8722322401]
 
 # Отдельные Telegram ID, которым разрешена команда /give в группе операторов.
 # Формат переменной окружения GIVE_TELEGRAM_IDS: "123,456". Если список пустой, /give доступна админам.
