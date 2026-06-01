1c2b6551bca627662ed42f087b196671c3b5abcb
lonixgrifer-afk <lonixgrifer@gmail.com>
2026-06-01 12:59:57 +0500

    Update max.py

--- max.py
+++ max.py
@@ -6,7 +6,7 @@ import time
 import urllib.error
 import urllib.parse
 import urllib.request
-from contextlib import closing
+from contextlib import closing 
 from datetime import datetime, timedelta, timezone
 
 
